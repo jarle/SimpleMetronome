099182f72e0c8f3bdfbd5d9f9aa17c77ff31465c
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2016-10-01 01:25:16 +0200

    added resources and time signature events

--- scripts/metronome.js
+++ scripts/metronome.js
@@ -1,17 +1,32 @@
 var metronomeRunning = false;
-var bpm = document.getElementById('bpmSlider').value;
-var visualComponent = null;
+var bpm = document.getElementById('bpmView').value;
+var tempoIndex = 1;
+var bpmRepeatingFunction = null;
 
 var bpmSlider = document.getElementById('bpmSlider')
 var bpmView = document.getElementById('bpmView')
 var metronomeButton = document.getElementById('metronomeButton')
-var metronomeVisualizer = document.getElementById('metronomeVisualizer')
+var timeSignatureUpperSelector = document.getElementById("timeSignatureUpper");
+var timeSignatureLowerSelector = document.getElementById("timeSignatureLower");
 
-var bpmChanged = new CustomEvent('bpmChanged');
+var bpmChangedEvent = new CustomEvent('bpmChanged');
+var timeSignatureChangedEvent = new CustomEvent('timeSignatureChanged');
 var metronomeToggledEvent = new CustomEvent('metronomeToggled');
 
+var FIRST_BEAT_NOTE_FILE = "res/whole_note.mp3"
+var BEAT_NOTE_FILE = "res/beat_note.mp3"
+
+var tempoArray = [
+                  new Audio(FIRST_BEAT_NOTE_FILE),
+                  new Audio(BEAT_NOTE_FILE),
+                  new Audio(BEAT_NOTE_FILE),
+                  new Audio(BEAT_NOTE_FILE)
+                ]
+var timeSignatureUpper = 4; //four per bar
+var timeSignatureLower = 4; //quarter notes
+
+
 bpmView.value = bpm;
-metronomeVisualizer.style.visibility = "hidden";
 
 function metronomeToggle(){
   metronomeRunning = !metronomeRunning;
@@ -20,28 +35,42 @@ function metronomeToggle(){
 
 function sliderChanged(){
   bpmView.value = bpmSlider.value;
-  document.dispatchEvent(bpmChanged);
+  document.dispatchEvent(bpmChangedEvent);
 }
 
 function valueChanged(){
   bpmSlider.value = bpmView.value;
-  document.dispatchEvent(bpmChanged);
+  document.dispatchEvent(bpmChangedEvent);
+}
+
+function updateTimeSignature(){
+  timeSignatureUpper = timeSignatureUpperSelector[timeSignatureUpperSelector.selectedIndex].value;
+  timeSignatureLower = timeSignatureLowerSelector[timeSignatureLowerSelector.selectedIndex].value;
+
+  var tempoRemainder = tempoArray.length - timeSignatureUpper;
+
+  for(; tempoRemainder > 0; tempoRemainder--){
+    tempoArray.pop();
+  }
+  for(; tempoRemainder < 0; tempoRemainder++){
+    tempoArray.push(new Audio(BEAT_NOTE_FILE));
+  }
+  document.dispatchEvent(timeSignatureChangedEvent)
 }
 
 function runMetronome(){
   var visualizationVisible = false;
 
   if(metronomeRunning){
-    var updateInterval = 1000*(60/(2*bpmView.value));
-    visualComponent = setInterval(function(e) {
+    var updateInterval = (1000*60*(4/timeSignatureLower))/bpmView.value; //1000 * 60 * 4/timesig * 1/frequency = period in ms
+    bpmRepeatingFunction = setInterval(function(e) {
       visualizationVisible = !visualizationVisible;
-      metronomeVisualizer.style.visibility = visualizationVisible ? "hidden" : "visible";
+      tempoArray[tempoIndex].play();
+      tempoIndex = (tempoIndex + 1) % tempoArray.length;
     }, updateInterval)
   }
   else {
-    clearInterval(visualComponent);
-    metronomeVisualizer.style.visibility = "hidden";
-    console.log("wtf");
+    clearInterval(bpmRepeatingFunction);
   }
 }
 
@@ -54,6 +83,11 @@ document.addEventListener('metronomeToggled', function(e){
 }, false);
 
 document.addEventListener('bpmChanged', function(e){
-  clearInterval(visualComponent);
+  clearInterval(bpmRepeatingFunction);
+  runMetronome();
+}, false);
+
+document.addEventListener('timeSignatureChanged', function(e){
+  clearInterval(bpmRepeatingFunction);
   runMetronome();
 }, false);
